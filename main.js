48f8d8e23c5ead2a9c3be5688bb6e748a4396d77
clark layos <clarklayos123@gmail.com>
2025-03-17 23:00:57 +0800

    update 2

--- main.js
+++ main.js
@@ -1,23 +1,21 @@
-// const colors = [
-//     "#FF5733", // Vibrant Red-Orange
-//     "#33FF57", // Bright Green
-//     "#3357FF", // Vivid Blue
-//     "#FF33A1", // Hot Pink
-//     "#A133FF", // Electric Purple
-//     "#33FFF5", // Aqua Blue
-//     "#F5FF33", // Neon Yellow
-//     "#FF8C33", // Bright Orange
-//     "#33FF8C", // Mint Green
-//     "#8C33FF"  // Deep Violet
-// ];
+const colors = [
+    "#FF5733", // Vibrant Red-Orange
+    "#33FF57", // Bright Green
+    "#3357FF", // Vivid Blue
+    "#FF33A1", // Hot Pink
+    "#A133FF", // Electric Purple
+    "#33FFF5", // Aqua Blue
+    "#F5FF33", // Neon Yellow
+    "#FF8C33", // Bright Orange
+    "#33FF8C", // Mint Green
+    "#8C33FF"  // Deep Violet
+];
 
-// function randomColor() {
-//     let random = Math.floor(Math.random() * colors.length);
-//     let colorsRandom = colors[random];
+function randomColor() {
+    let random = Math.floor(Math.random() * colors.length);
+    let colorsRandom = colors[random];
 
-//     document.querySelectorAll(".skillsContent").forEach(element => {
-//         element.style.boxShadow = `0 0 100px ${colorsRandom}`;
-//     });
+    document.getElementById("skillBorder").style.boxShadow = `0 0 50px ${colorsRandom}`;
 
-// }
-// setInterval(randomColor, 500);
+}
+setInterval(randomColor, 300);
